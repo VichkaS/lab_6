f9afd9cccb8ed91ff62d7f76a3bc8610584a0616
VichkaS <vi4ka.vs@gmail.com>
2015-05-14 18:58:43 +0300

    задание 1

---
+++ oop6_1/Solve3.cpp
@@ -0,0 +1,86 @@
+#include "stdafx.h"
+#define _USE_MATH_DEFINES
+#include "math.h"
+#include "Solve3.h"
+#include <stdexcept>
+
+using namespace std;
+
+EquationRoots3 GetResultWhenDLessZero(double a, double b, double p, double q, double D)
+{
+	EquationRoots3 result;
+
+	double angle;
+	if (q < 0)
+	{
+		angle = atan(sqrt(-D) / (-q / 2));
+	}
+	if (q > 0)
+	{
+		angle = atan(sqrt(-D) / (-q / 2)) + M_PI;
+	}
+	if (q == 0)
+	{
+		angle = M_PI / 2;
+	}
+	double y1, y2, y3;
+	y1 = 2 * sqrt(-p / 3) * cos(angle / 3);
+	y2 = 2 * sqrt(-p / 3) * cos((angle + 2 * M_PI) / 3);
+	y3 = 2 * sqrt(-p / 3) * cos((angle + 4 * M_PI) / 3);
+	result.numRoots = 3;
+	result.roots[0] = y1 - (b / 3 * a);
+	result.roots[1] = y2 - (b / 3 * a);
+	result.roots[2] = y3 - (b / 3 * a);
+	return result;
+}
+EquationRoots3 GetResultWhenDMoreZero(double a, double b, double q, double D)
+{
+	EquationRoots3 result;
+	double y1;
+	y1 = pow((-q / 2) + sqrt(D), 1.0 / 3.0) - pow((q / 2) + sqrt(D), 1.0 / 3.0);
+	result.numRoots = 1;
+	result.roots[0] = y1 - (b / 3 * a);
+	return result;
+}
+
+EquationRoots3 GetResultWhenDEqualZero(double a, double b, double q, double D)
+{
+	EquationRoots3 result;
+	double y1, y2;
+	y1 = -2 * pow(q / 2, 1.0 / 3.0);
+	y2 = pow(q / 2, 1.0 / 3.0);
+	result.numRoots = 2;
+	result.roots[0] = y1 - (b / 3 * a);
+	result.roots[1] = y2 - (b / 3 * a);
+	return result;
+}
+
+// Вычисляем корни кубического уравнения ax^3 + bx^2 + cx + d = 0
+// формула Кардано
+EquationRoots3 Solve3(double a, double b, double c, double d)
+{
+	if (a == 0)
+	{
+		throw std::invalid_argument("the argument must be = 0");
+	}
+
+	EquationRoots3 result;
+	double p = ((3 * a * c) - (b * b)) / 3 * a * a;
+	double q = (2 * b * b * b - 9 * a * b * c + 27 * a * a * d) / 27 * a * a * a;
+	double D = (q / 2) * (q / 2) + (p / 3) * (p / 3) * (p / 3);
+
+	if (D < 0)
+	{
+		result = GetResultWhenDLessZero(a, b, p, q, D);
+	}
+
+	if (D > 0)
+	{
+		result = GetResultWhenDMoreZero(a, b, q, D);
+	}
+	if (D == 0)
+	{
+		result = GetResultWhenDEqualZero(a, b, q, D);
+	}
+	return result;
+}
---
+++ oop6_1/oop6_1.cpp
@@ -0,0 +1,39 @@
+// oop6_1.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include "Solve3.h"
+//#include <stdexcept>
+#include <iostream>
+using namespace std;
+
+void PrintResult(EquationRoots3 const &result)
+{
+	for (int i = 0; i < result.numRoots; i++)
+	{
+		cout << "x" << i + 1 << " = " << result.roots[i] << endl;
+	}
+}
+int main()
+{
+	//double a = 1, b = 6, c = 3, d = -10; // D < 0
+	//double a = 1, b = 12, c = 3, d = 4;
+	double a = 1, b = 12, c = 36, d = 32; // D = 0
+	EquationRoots3 result;
+	result.numRoots = 0;
+	result.roots[0] = 0;
+	result.roots[1] = 0;
+	result.roots[2] = 0;
+	try
+	{		
+		//cin >> a >> b >> c >> d;
+		result = Solve3(a, b, c, d);
+		PrintResult(result);
+	}
+	catch (std::invalid_argument const & e) 
+	{
+		cout << "Error: " << e.what() << "\n";
+	}
+	return 0;
+}
+
